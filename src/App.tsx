dd86dbf43337d32724defc313e2251214e7af5e7
Reossann <s1f102502283@iniad.org>
2026-08-03 12:54:47 +0900

    fix: app.tsxのほうも相対パスに

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { AppLayout } from "@/components/layout/AppLayout";
-import { useNavigation } from "@/hooks/useNavigation";
-import { DashboardPage } from "@/pages/DashboardPage";
-import { HistoryPage } from "@/pages/HistoryPage";
-import { RulesPage } from "@/pages/RulesPage";
-import { SettingsPage } from "@/pages/SettingsPage";
+import { AppLayout } from "./components/layout/AppLayout";
+import { useNavigation } from "./hooks/useNavigation";
+import { DashboardPage } from "./pages/DashboardPage";
+import { HistoryPage } from "./pages/HistoryPage";
+import { RulesPage } from "./pages/RulesPage";
+import { SettingsPage } from "./pages/SettingsPage";
 
 const pageMap = {
   dashboard: <DashboardPage />,
