e66e02d35b0b196933fc2723942d5173edaea367
K300000 <s1f102602958@iniad.org>
2026-08-04 15:56:40 +0900

    自動起動プラグインをセットアップ

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,18 @@ pub mod services;
 
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::Builder::new().build())
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                use tauri_plugin_autostart::MacosLauncher;
+
+                let _ = app.handle().plugin(tauri_plugin_autostart::init(
+                    MacosLauncher::LaunchAgent,
+                    Some(vec!["--flag1", "--flag2"]),
+                ));
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("Tauri アプリの起動に失敗しました");
 }
