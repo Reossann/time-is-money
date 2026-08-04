b9883898573d7f581dd02a3ab3f7cdc600d1ba32
K300000 <s1f102602958@iniad.org>
2026-08-04 23:38:19 +0900

    アプリマウント時にタイマーを自動開始

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { AppLayout } from "./components/layout/AppLayout";
 import { useNavigation } from "./hooks/useNavigation";
+import { useActivityStore } from "./stores/useActivityStore";
 import { DashboardPage } from "./pages/DashboardPage";
 import { HistoryPage } from "./pages/HistoryPage";
 import { RulesPage } from "./pages/RulesPage";
@@ -14,6 +16,19 @@ const pageMap = {
 
 export function App() {
   const { currentPage, setCurrentPage } = useNavigation();
+
+  // タイマーロジック：アプリマウント時に、1秒ごとに increment を実行
+  useEffect(() => {
+    const { isRunning } = useActivityStore.getState();
+    if (!isRunning) return;
+
+    const intervalId = setInterval(() => {
+      useActivityStore.getState().increment();
+    }, 1000);
+
+    // クリーンアップ：コンポーネントアンマウント時に clearInterval
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <AppLayout currentPage={currentPage} onNavigate={setCurrentPage}>
